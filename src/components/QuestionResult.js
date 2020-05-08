58dacea417a7ba4177a14587613e2d33e5988837
marynapl <kushnirmarinka@gmail.com>
2020-05-08 13:32:37 -0400

    Add Question Results component

--- src/components/QuestionResult.js
+++ src/components/QuestionResult.js
@@ -1,18 +1,69 @@
-import React, { Component } from 'react'
+import React, { Component, Fragment } from 'react'
 import { connect } from 'react-redux'
 
 class QuestionResult extends Component {
   render() {
-    const { question } = this.props
+    const { question, userAnswer } = this.props
+
+    const optionOneAnswers = question.optionOne.votes.length
+    const optionTwoAnswers = question.optionTwo.votes.length
+    const totalAnswers = optionOneAnswers + optionTwoAnswers
+    const optionOnePercent = (optionOneAnswers / totalAnswers) * 100
+    const optionTwoPercent = (optionTwoAnswers / totalAnswers) * 100
+
+    // TODO: there is an opportunity to create a separate QuestionResultOption component
+
     return (
-      <div>Results</div>
+      <Fragment>
+        <p className="text-bold">Results</p>
+        <div className={userAnswer === "optionOne" ? "callout secondary" : "callout"}>
+          {userAnswer === "optionOne"
+            ? <div className="badge question-badge">You vote</div>
+            : null
+          }
+          <p>Would you rather {question.optionOne.text} ?</p>
+          <div className="progress" role="progressbar" tabIndex="0" aria-valuenow="25" aria-valuemin="0" aria-valuetext="25 percent" aria-valuemax="100">
+            <span className="progress-meter" style={{ width: optionOnePercent + '%' }} >
+              {optionOnePercent > 0
+                ? <span className="progress-meter-text">{optionOnePercent}%</span>
+                : null
+              }
+            </span>
+          </div>
+          <p className="text-center text-bold text-small">
+            {optionOneAnswers} out of {totalAnswers} votes
+          </p>
+        </div>
+        <div className={userAnswer === "optionTwo" ? "callout secondary" : "callout"}>
+          {userAnswer === "optionTwo"
+            ? <div className="badge question-badge">You vote</div>
+            : null
+          }
+          <p>Would you rather {question.optionTwo.text} ?</p>
+          <div className="progress" role="progressbar" tabIndex="0" aria-valuenow="25" aria-valuemin="0" aria-valuetext="25 percent" aria-valuemax="100">
+            <span className="progress-meter" style={{ width: optionTwoPercent + '%' }} >
+              {optionTwoPercent > 0
+                ? <span className="progress-meter-text">{optionTwoPercent}%</span>
+                : null
+              }
+            </span>
+          </div>
+          <p className="text-center text-bold text-small">
+            {optionTwoAnswers} out of {totalAnswers} votes
+          </p>
+        </div>
+      </Fragment>
     )
   }
 }
-const mapStateToProps = ({ questions, authedUser }, { id }) => {
+
+const mapStateToProps = ({ questions, authedUser, users }, { id }) => {
+  const question = questions[id]
+  const userAnswer = users[authedUser].answers[question.id]
+
   return {
-    authedUser,
-    question: questions[id]
+    userAnswer,
+    question
   }
 }
 
